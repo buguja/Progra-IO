2f5f33f63d3eb99eb8116ec4039e4ab6934a0d55
Luis Diego <ldiegopizarro@gmail.com>
2015-05-01 14:17:20 -0600

    Problema Distribucion
    Ya funcionan todos los pasos, falta el final que es evaluar la matriz.Y
    también falta el paso de cuando es maximizar restarle a toda la matriz

--- Transporte/Transp.py
+++ Transporte/Transp.py
@@ -1,3 +1,4 @@
+import sys
 __author__ = 'luisdiegopizarro'
 
 #se va considerar una celda como todo un conjunto de celas llamese una fila o una columna
@@ -14,8 +15,6 @@ class Celdas:
 #salida:arreglo de celdas ordenado por mayor cantidad de 0's
 def contarCeros(matriz):
     largo=len(matriz)
-    filas=[]
-    columnas=[]
     celdas=[]
     #cuenta los 0's en las filas
     for f in range(0,largo):
@@ -40,17 +39,20 @@ def contarCeros(matriz):
 #
 def marcarLineas(matriz):
     arregloCerosContados=contarCeros(matriz)
-    printCeldas(arregloCerosContados)
-    celdasMarcadas=[]
+    filasMarcadas=[]
+    columnasMarcadas=[]
 
     while(arregloCerosContados[0].cantidadCeros!=0):
         celdaMarcada=arregloCerosContados[0]
-        celdasMarcadas.append([celdaMarcada.orientacion,celdaMarcada.posicion])
+        if(celdaMarcada.orientacion=='fila'):
+            filasMarcadas.append(celdaMarcada.posicion)
+        else:
+            columnasMarcadas.append(celdaMarcada.posicion)
         eliminarCeros(arregloCerosContados[1:],arregloCerosContados[0])
         arregloCerosContados=arregloCerosContados[1:]
         quick_sort(arregloCerosContados)
+    return [filasMarcadas,columnasMarcadas]
 
-    print(celdasMarcadas)
 
 #entra el arreglo de celdas y la celda que se esta marcando
 #salida: el arreglo celdas sin la celda marcada y con las otras celdas modificadas
@@ -62,8 +64,6 @@ def eliminarCeros(arregloCeldas,celdaMarcada):
             if(celda.orientacion!=orientacionBorrar and celda.posicion==posicion):
                 celda.cantidadCeros-=1
                 celda.posicionCeros.remove(posicionBorrar)
-
-
 
 #quicksort descendiente especial para la clase Celda
 def quick_sort(items):
@@ -92,7 +92,62 @@ def printCeldas(items):
 
 
 
-marcarLineas([[0,6,1,0],[5,0,0,7],[5,0,0,7],[0,1,5,5]])
+#marcarLineas([[0,6,1,0],[5,0,0,7],[5,0,0,7],[0,1,5,5]])
+
+#resta todas las filas su menor elemento respectivamente
+def menorFila(matriz):
+    largo=len(matriz)
+    for f in range(0,largo):
+        menor=matriz[f][0]
+        for c in range(0,largo):
+            if(matriz[f][c]<menor):menor=matriz[f][c]
+        for r in range(0,largo):matriz[f][r]=matriz[f][r]-menor
+
+    return matriz
+#resta a todas las columnas su menor elemento respectivamente
+def menorColumna(matriz):
+    largo=len(matriz)
+    for c in range(0,largo):
+        menor=matriz[0][c]
+
+        for f in range(0,largo):
+            if(matriz[f][c]<menor):menor=matriz[f][c]
+        for r in range(0,largo):matriz[r][c]=matriz[r][c]-menor
+    return matriz
+
+#resta el menor valor no marcado, a todos los valores NO marcados
+#se lo suma a los valores que esten en las intersecciones
+def operaMenor(matriz,celdasMarcadas):
+    filas=celdasMarcadas[0]
+    columnas=celdasMarcadas[1]
+    menor=findMenor(matriz,celdasMarcadas)
+    for i,fila in enumerate(matriz):
+            for j,val in enumerate(fila):
+                if((not i in filas)and (not j in columnas)):
+                    matriz[i][j]=val-menor
+                if(i in filas and j in columnas):
+                    matriz[i][j]=val+menor
+    return matriz
+
+#encuentra el menor valor de las filas y columnas que no estan marcadas
+def findMenor(matriz,celdasMarcadas):
+    filas=celdasMarcadas[0]
+    columnas=celdasMarcadas[1]
+    menor=sys.maxsize
+    for i,fila in enumerate(matriz):
+        if(not i in filas):
+            for j,val in enumerate(fila):
+                if(not j in columnas):
+                    if(val<menor):menor=val
+    return menor
+
+
+
+
+
+a=menorColumna(menorFila([[10,9,5],[9,8,3],[6,4,7]]))
+b=marcarLineas(a)
+print(operaMenor(a,b))
 
 
 
